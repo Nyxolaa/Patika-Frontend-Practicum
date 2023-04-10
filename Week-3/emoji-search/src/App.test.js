8bf618c461f99d089a5a31d83b43c0a60bfceba7
Nyxola <88090162+Nyxolaa@users.noreply.github.com>
2023-04-10 18:46:29 +0300

    initial commit

--- Week-3/emoji-search/src/App.test.js
+++ Week-3/emoji-search/src/App.test.js
@@ -1,8 +1,12 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-it("renders without crashing", () => {
+it("renders title without crashing", () => {
   const div = document.createElement("div");
   ReactDOM.render(<App />, div);
 });
+ 
